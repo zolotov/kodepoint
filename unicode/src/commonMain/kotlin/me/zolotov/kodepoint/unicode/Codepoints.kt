da3d7634519a7cd75b6ca6f98ff1d6338db40bad
Alexander Zolotov <zolotov@jetbrains.com>
2026-01-12 12:59:00 +0100

    Adapt codepoint to the generated classes

--- unicode/src/commonMain/kotlin/me/zolotov/kodepoint/unicode/Codepoints.kt
+++ unicode/src/commonMain/kotlin/me/zolotov/kodepoint/unicode/Codepoints.kt
@@ -4,33 +4,65 @@ import me.zolotov.kodepoint.generated.CharacterData
 import me.zolotov.kodepoint.generated.ScriptData
 import me.zolotov.kodepoint.internal.asciiToLowerCase
 import me.zolotov.kodepoint.internal.asciiToUpperCase
+import me.zolotov.kodepoint.internal.binarySearchRange
 import me.zolotov.kodepoint.internal.isAscii
 import me.zolotov.kodepoint.script.UnicodeScript
 
+/**
+ * Unicode character property functions.
+ */
 object Codepoints {
+
+    // Pre-computed masks for efficient multi-category checks
+    private const val LETTER_MASK = (1 shl CharacterData.CAT_LU) or (1 shl CharacterData.CAT_LL) or
+        (1 shl CharacterData.CAT_LT) or (1 shl CharacterData.CAT_LM) or (1 shl CharacterData.CAT_LO)
+    private const val LETTER_OR_DIGIT_MASK = LETTER_MASK or (1 shl CharacterData.CAT_ND)
+    private const val SPACE_CHAR_MASK = (1 shl CharacterData.CAT_ZS) or (1 shl CharacterData.CAT_ZL) or
+        (1 shl CharacterData.CAT_ZP)
+
+    // Helper functions for property interpretation
+    private fun getCategory(props: Int): Int =
+        (props and CharacterData.CATEGORY_MASK) ushr CharacterData.CATEGORY_SHIFT
+
+    private fun getCaseDelta(props: Int): Int {
+        val delta = props and CharacterData.CASE_DELTA_MASK
+        return if (delta >= 0x200) delta - 0x400 else delta
+    }
+
+    private fun isDeltaToLowercase(props: Int): Boolean =
+        (props and CharacterData.DELTA_TO_LOWERCASE_BIT) != 0
+
+    private fun hasLargeLowercaseDelta(props: Int): Boolean =
+        (props and CharacterData.HAS_LARGE_LOWERCASE_DELTA_BIT) != 0
+
+    private fun hasLargeUppercaseDelta(props: Int): Boolean =
+        (props and CharacterData.HAS_LARGE_UPPERCASE_DELTA_BIT) != 0
+
     fun isLetter(codepoint: Int): Boolean {
-        val properties = CharacterData.getProperties(codepoint)
-        return CharacterData.isLetter(properties)
+        val props = CharacterData.getProperties(codepoint)
+        return ((1 shl getCategory(props)) and LETTER_MASK) != 0
     }
 
     fun isDigit(codepoint: Int): Boolean {
-        val properties = CharacterData.getProperties(codepoint)
-        return CharacterData.isDigit(properties)
+        val props = CharacterData.getProperties(codepoint)
+        return getCategory(props) == CharacterData.CAT_ND
     }
 
     fun isLetterOrDigit(codepoint: Int): Boolean {
-        val properties = CharacterData.getProperties(codepoint)
-        return CharacterData.isLetterOrDigit(properties)
+        val props = CharacterData.getProperties(codepoint)
+        return ((1 shl getCategory(props)) and LETTER_OR_DIGIT_MASK) != 0
     }
 
     fun isUpperCase(codepoint: Int): Boolean {
-        val properties = CharacterData.getProperties(codepoint)
-        return CharacterData.isUpperCase(properties)
+        val props = CharacterData.getProperties(codepoint)
+        return getCategory(props) == CharacterData.CAT_LU ||
+            (props and CharacterData.IS_OTHER_UPPERCASE_BIT) != 0
     }
 
     fun isLowerCase(codepoint: Int): Boolean {
-        val properties = CharacterData.getProperties(codepoint)
-        return CharacterData.isLowerCase(properties)
+        val props = CharacterData.getProperties(codepoint)
+        return getCategory(props) == CharacterData.CAT_LL ||
+            (props and CharacterData.IS_OTHER_LOWERCASE_BIT) != 0
     }
 
     fun toLowerCase(codepoint: Int): Int {
@@ -38,16 +70,14 @@ object Codepoints {
             return asciiToLowerCase(codepoint)
         }
 
-        val properties = CharacterData.getProperties(codepoint)
+        val props = CharacterData.getProperties(codepoint)
 
-        // Check for large delta flag first - avoids redundant isUpperCase check
-        if (CharacterData.hasLargeLowercaseDelta(properties)) {
-            return CharacterData.getLargeLowercase(codepoint)
+        if (hasLargeLowercaseDelta(props)) {
+            return codepoint + binarySearchRange(codepoint, CharacterData.largeLowercaseRanges, 0)
         }
 
-        val delta = CharacterData.getCaseDelta(properties)
-        // If we have a non-zero delta with toLowerCase direction, use it
-        return if (delta != 0 && CharacterData.isDeltaToLowercase(properties)) {
+        val delta = getCaseDelta(props)
+        return if (delta != 0 && isDeltaToLowercase(props)) {
             codepoint + delta
         } else {
             codepoint
@@ -59,24 +89,22 @@ object Codepoints {
             return asciiToUpperCase(codepoint)
         }
 
-        val properties = CharacterData.getProperties(codepoint)
+        val props = CharacterData.getProperties(codepoint)
 
         // Special handling for titlecase letters (Lt)
-        if (CharacterData.isTitleCase(properties)) {
-            // Latin Extended-B titlecase letters: 01C5, 01C8, 01CB, 01F2
+        if (getCategory(props) == CharacterData.CAT_LT) {
             return when (codepoint) {
                 0x01C5, 0x01C8, 0x01CB, 0x01F2 -> codepoint - 1
-                else -> codepoint // Greek titlecase letters have no uppercase
+                else -> codepoint
             }
         }
 
-        if (CharacterData.hasLargeUppercaseDelta(properties)) {
-            return CharacterData.getLargeUppercase(codepoint)
+        if (hasLargeUppercaseDelta(props)) {
+            return codepoint + binarySearchRange(codepoint, CharacterData.largeUppercaseRanges, 0)
         }
 
-        val delta = CharacterData.getCaseDelta(properties)
-        // If we have a non-zero delta with toUpperCase direction, use it
-        return if (delta != 0 && !CharacterData.isDeltaToLowercase(properties)) {
+        val delta = getCaseDelta(props)
+        return if (delta != 0 && !isDeltaToLowercase(props)) {
             codepoint + delta
         } else {
             codepoint
@@ -84,51 +112,50 @@ object Codepoints {
     }
 
     fun isSpaceChar(codepoint: Int): Boolean {
-        val properties = CharacterData.getProperties(codepoint)
-        return CharacterData.isSpaceChar(properties)
+        val props = CharacterData.getProperties(codepoint)
+        return ((1 shl getCategory(props)) and SPACE_CHAR_MASK) != 0
     }
 
     fun isWhitespace(codepoint: Int): Boolean {
-        val properties = CharacterData.getProperties(codepoint)
-        return CharacterData.isWhitespace(properties)
+        val props = CharacterData.getProperties(codepoint)
+        return (props and CharacterData.IS_WHITESPACE_BIT) != 0
     }
 
     fun isIdeographic(codepoint: Int): Boolean {
-        val properties = CharacterData.getProperties(codepoint)
-        return CharacterData.isIdeographic(properties)
+        val props = CharacterData.getProperties(codepoint)
+        return (props and CharacterData.IS_IDEOGRAPHIC_BIT) != 0
     }
 
     fun isIdentifierIgnorable(codepoint: Int): Boolean {
-        // 0x00-0x08, 0x0E-0x1B are identifier-ignorable in ASCII
-        // 0x7F-0x9F are identifier-ignorable control characters
         if (codepoint <= 0x08 || (codepoint in 0x0E..0x1B) || (codepoint in 0x7F..0x9F)) {
             return true
         }
-        val properties = CharacterData.getProperties(codepoint)
-        return CharacterData.isFormatChar(properties)
+        val props = CharacterData.getProperties(codepoint)
+        return getCategory(props) == CharacterData.CAT_CF
     }
 
     fun isUnicodeIdentifierStart(codepoint: Int): Boolean {
-        val properties = CharacterData.getProperties(codepoint)
-        return CharacterData.isUnicodeIdentifierStart(properties)
+        val props = CharacterData.getProperties(codepoint)
+        return (props and CharacterData.IS_UNICODE_ID_START_BIT) != 0
     }
 
     fun isUnicodeIdentifierPart(codepoint: Int): Boolean {
-        val properties = CharacterData.getProperties(codepoint)
-        return CharacterData.isUnicodeIdentifierPart(properties)
+        val props = CharacterData.getProperties(codepoint)
+        return (props and CharacterData.IS_UNICODE_ID_PART_BIT) != 0
     }
 
     fun isJavaIdentifierStart(codepoint: Int): Boolean {
-        val properties = CharacterData.getProperties(codepoint)
-        return CharacterData.isJavaIdentifierStart(properties)
+        val props = CharacterData.getProperties(codepoint)
+        return (props and CharacterData.IS_JAVA_ID_START_BIT) != 0
     }
 
     fun isJavaIdentifierPart(codepoint: Int): Boolean {
-        val properties = CharacterData.getProperties(codepoint)
-        return CharacterData.isJavaIdentifierPart(properties)
+        val props = CharacterData.getProperties(codepoint)
+        return (props and CharacterData.IS_JAVA_ID_PART_BIT) != 0
     }
 
-    fun isISOControl(codepoint: Int): Boolean = codepoint in 0x00..0x1F || codepoint in 0x7F..0x9F
+    fun isISOControl(codepoint: Int): Boolean =
+        codepoint in 0x00..0x1F || codepoint in 0x7F..0x9F
 
     fun getUnicodeScript(codepoint: Int): UnicodeScript = ScriptData.getScript(codepoint)
 }
